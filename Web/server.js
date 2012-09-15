1f721cef2ff9d30eeb0d21cf0e51e5de267d95f5
Amir Rajan <ar@amirrajan.net>
2012-09-14 22:07:53 -0500

    added express and returning of view

--- Web/server.js
+++ Web/server.js
@@ -1,12 +1,20 @@
-var app = require('express')()
+var express = require('express');
+var app = express()
   , server = require('http').createServer(app)
 
 server.listen(80);
 
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.use(express.methodOverride());
+app.use(express.bodyParser());  
+app.use(app.router);
+app.use('/public', express.static('public'));
+
 var games = [];
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/list', function (req, res) {
