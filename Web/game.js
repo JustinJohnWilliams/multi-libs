a6836224d6c56968443b59debe495750e5ac4422
Amir Rajan <ar@amirrajan.net>
2012-09-15 10:23:40 -0500

    added current card

--- Web/game.js
+++ Web/game.js
@@ -4,7 +4,7 @@ var linq = require('linq');
 var gameList = [];
 
 var deck = {
-  black:  ["A", "B", "C", "D", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  black:  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
   white: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27",
 "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"] 
 };
@@ -16,6 +16,7 @@ function list() {
 }
 
 function addGame(game) {
+  game.id = game.gameId;
   game.players = [];
   game.isStarted = false;
   game.deck = _.clone(deck);
@@ -31,10 +32,17 @@ function joinGame(game, player) {
   game.players.push({ id: player.id, name: player.name });
 
   if(game.players.length == 4) {
-    game.isStarted = true;
+    startGame(game);
   }
 
   return game;
+}
+
+function startGame(game) {
+    game.isStarted = true;
+    var index = Math.floor(Math.random() * game.deck.black.length);
+    game.currentBlackCard = game.deck.black[index];
+    game.deck.black.splice(index, 1);
 }
 
 exports.list = list;
--- Web/specifications/describe_Game.js
+++ Web/specifications/describe_Game.js
@@ -1,7 +1,26 @@
 var _ = require('underscore');
 var Game = require('../game.js')
-describe('test', function() {
-  return it('works', function() {
-    return expect(Game.list().length).toBe(0);
+describe('multi-libs', function() {
+  function startGame(gameId) {
+    var game = Game.addGame({ gameId: gameId, name: "somename" });
+    expect(Game.list().length).toBe(1);
+    game = Game.getGame(gameId);
+    Game.joinGame(game, { id: "player1", name: "player1" });
+    Game.joinGame(game, { id: "player2", name: "player2" });
+    Game.joinGame(game, { id: "player3", name: "player3" });
+    Game.joinGame(game, { id: "player4", name: "player4" });
+    game = Game.getGame(gameId);
+    return game;
+  }
+
+  it('game started', function() {
+    var game = startGame("newgame");
+    expect(game.isStarted).toBe(true);
+  });
+
+  it('game started, black card selected', function() {
+    var game = startGame("newgame");
+    expect(game.currentBlackCard).toBeTruthy();
+    expect(game.deck.black.length).toBe(25);
   });
 });
