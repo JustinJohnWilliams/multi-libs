4174250b2403191926cf6c234f937933eab527d8
JustinJohnWilliams <JustinJohnWilliams@gmail.com>
2012-09-14 22:20:32 -0500

    after merge

--- Web/server.js
+++ Web/server.js
@@ -17,6 +17,10 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.get('/game', function (req, res) {
+	res.render('game');
+});
+
 app.get('/list', function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify({ games: [ { game: { name: "Name" } } ] }));
