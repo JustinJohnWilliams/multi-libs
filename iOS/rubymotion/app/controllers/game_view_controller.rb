8f2339d75e32fde780b2428fb1fef7d55a97c8d9
Clifton King <cliftonk@gmail.com>
2012-09-18 21:13:27 -0500

    added cards from deck to GameViewController

--- iOS/rubymotion/app/controllers/game_view_controller.rb
+++ iOS/rubymotion/app/controllers/game_view_controller.rb
@@ -5,11 +5,38 @@ class GameViewController < UIViewController
     super
 
     self.title = game['name']
+
+    @table = UITableView.alloc.initWithFrame(self.view.bounds)
+    self.view.addSubview @table
+
+    @table.dataSource = self
+    @table.delegate = self
+
+    @cards = game['deck'].values.flatten
+
+    puts game.inspect
   end
 
   def initWithGame(game)
     initWithNibName(nil, bundle: nil)
     self.game = game
     self
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) ||
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault,
+                                          reuseIdentifier: @reuseIdentifier)
+
+    card = @cards[indexPath.row]
+    cell.textLabel.text = card
+
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @cards.size
   end
 end
