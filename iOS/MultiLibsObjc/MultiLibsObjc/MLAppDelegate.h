533e98c36a0e2b883dfbb3707286690b0a16c54b
Amir Rajan <ar@amirrajan.net>
2012-09-29 14:31:48 -0500

    create objc file

---
+++ iOS/MultiLibsObjc/MultiLibsObjc/MLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLAppDelegate.h
+//  MultiLibsObjc
+//
+//  Created by Amirali Rajan on 9/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
