ef9ff9fae13d627a9d543e83f647d98e53773b0b
Amir Rajan <ar@amirrajan.net>
2012-10-01 21:25:36 -0500

    added hello world app for objective c

---
+++ iOS/HelloWorldObjC/HelloWorldObjC/HelloWorldViewController.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorldObjC
+//
+//  Created by Amirali Rajan on 10/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController 
+@property (weak, nonatomic) IBOutlet UIButton *buttonSayHello;
+@property (weak, nonatomic) IBOutlet UILabel *labelSayHello;
+- (IBAction)buttonSayHelloClicked:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *buttonCount;
+@property (nonatomic) NSInteger count;
+
+@end
