bf2ef213e6fa8c84c92c5b08b4256a9974ce392e
Amir Rajan <ar@amirrajan.net>
2012-10-04 18:50:44 -0500

    got hello rest working

---
+++ iOS/HelloRest/HelloRestClient/HelloRestClient/PeopleViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeopleViewController.h
+//  HelloRestClient
+//
+//  Created by Amirali Rajan on 10/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeopleViewController : UIViewController
+
+@end
