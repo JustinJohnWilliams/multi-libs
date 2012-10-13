b37ea6addd20c7c8cd08c45c2f4a1274adffdce5
Amir Rajan <ar@amirrajan.net>
2012-10-13 08:59:35 -0500

    added media queriy bare bone

---
+++ MediaQueries/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express()
+var server = require('http').createServer(app);
+
+server.listen(process.env.PORT || 3000);
+
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.use(express.methodOverride());
+app.use(express.bodyParser());  
+app.use(app.router);
+app.use('/public', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
